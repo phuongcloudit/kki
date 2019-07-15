627264c1c8ba23de05e06de3e1c1053f516ed533
Phuong <dongphuong2102@gmail.com>
2019-07-15 17:34:04 +0700

    request fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,8 @@
 $(document).ready(function($){
     $("#menu").click(function(){
       $("header").toggleClass("active");
-    })
+    });
 })
+function myFunction() {
+  jQuery("#document-1").attr('checked', true);
+}
